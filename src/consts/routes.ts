8edbe37c67f5a662bd821e8310628e88af390be9
Jhonny Stiven Agudelo Tenorio <jhonny.agudelo3120@gmail.com>
2024-12-09 21:58:30 -0500

    se crea la pagina de informacion

---
+++ src/consts/routes.ts
@@ -0,0 +1,11 @@
+export const PublicRoutes: AstroRoute = {
+  LOGIN: "login",
+  PUBLIC: "public",
+  REGISTER: "register",
+  INFORMATION: "information",
+};
+
+export const ProtectedRoutes: AstroRoute = {
+  PROTECTED: "private",
+  PRIVATE: "private",
+};
